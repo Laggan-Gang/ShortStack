30062358234f24ad8f7546dc3a9607e756d6836c
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-08-27 23:10:24 +0200

    Yeehaw

---
+++ shortStack.js
@@ -0,0 +1,55 @@
+//möjligtvis bra inv-länk https://discord.com/api/oauth2/authorize?client_id=1011640018479091722&permissions=328568269888&scope=bot%20applications.commands
+
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+}
+client.once("ready", () => {
+    console.log("ShortStack!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+    const command = client.commands.get(interaction.commandName);
+
+    if (!interaction.isChatInputCommand()) return;
+    if (!rightPlaceChecker(interaction)) return;
+    if (command) {
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+                components: [],
+            });
+        }
+    }
+});
+
+client.login(token);
+
+//For whenever I manage to make a config file and stuff
+function rightPlaceChecker(interaction) {
+    if (interaction.channelId != "539847809004994560") {
+        interaction.reply(
+            `Please handle these kinds of things in ${interaction.guild.channels.cache.get("539847809004994560").toString()}!\nLove, ShortStack!`
+        );
+        return false;
+    } else {
+        return true;
+    }
+}
