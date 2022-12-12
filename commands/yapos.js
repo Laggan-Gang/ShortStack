db9c97d2d5273cf276d9aa9a5d389d825030cbae
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-12-13 00:22:09 +0100

    Update yapos.js

--- commands/yapos.js
+++ commands/yapos.js
@@ -259,9 +259,9 @@ function readyEmbed(readyArray) {
   const playerFields = [];
   for (let player of readyArray) {
     if (player.ready) {
-      playerFields.push(player.user.username + "✅");
+      playerFields.push(player.username + "✅");
     } else {
-      playerFields.push(player.user.username + "❌");
+      playerFields.push(player.username + "❌");
     }
   }
   const embed = {
