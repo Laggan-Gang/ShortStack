dbd4c7faf20f85cfe3bb5f1ebea8ba8f7ef46a8d
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-12-16 00:52:41 +0100

    It's done and it's beautiful

--- commands/yapos.js
+++ commands/yapos.js
@@ -64,7 +64,7 @@ async function setUp(interaction, confirmedPlayers) {
   const inOutButtons = inOut();
   const time = getTimestamp(1000);
   const message = await interaction.channel.send({
-    content: `Yapos call, closes <t:${time + ONEHOUR}:R>`, //<@&412260353699872768> yapos
+    content: `<@&412260353699872768> call, closes <t:${time + ONEHOUR}:R>`, //<@&412260353699872768> yapos
     embeds: [embed],
     components: [inOutButtons],
   });
