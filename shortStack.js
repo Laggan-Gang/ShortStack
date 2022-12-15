46d3077a23cef8d976a026583fdf12288e6defa7
snygghugo <91157834+snygghugo@users.noreply.github.com>
2022-12-16 00:44:43 +0100

    Update shortStack.js

--- shortStack.js
+++ shortStack.js
@@ -2,7 +2,12 @@
 
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Intents,
+} = require("discord.js");
 const { token } = require("./config.json");
 
 const client = new Client({
