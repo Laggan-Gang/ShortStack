0cc83ded44f697ffeb6cefc13e76083ddd9725db
snygghugo <91157834+snygghugo@users.noreply.github.com>
2023-04-27 21:54:20 +0200

    Update littleHelper.js

--- littleHelper.js
+++ littleHelper.js
@@ -10,16 +10,16 @@ app.get("/queue", (req, res) => {
 });
 
 app.post("/queue", (req, res) => {
-  const dev = {
-    id: db.length + 1,
-    name: req.body.name,
-    email: req.body.email,
+  const queuer = {
+    id: req.body.id,
   };
-  db.push(dev);
-  res.status(201).header("location", `queue`).json(dev).end();
+  queue.push(queuer);
+  res.status(201).header("location", `queue`).json(queue).end();
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+//curl -H "Content-Type: application/json" -d '{"id":"my ID string",}' http://localhost:3000/queue
