67bbea6db5e920dfe255fa5c862d9a69b9797792
snygghugo <91157834+snygghugo@users.noreply.github.com>
2023-04-27 16:46:32 +0200

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -5,9 +5,11 @@ module.exports = {
     .setName("queue")
     .setDescription("Time to queue"),
   async execute(interaction) {
-    interaction.reply({ content: "You're in Q position: ", ephemeral: true });
+    await interaction.reply({
+      content: "You're in Q position: ",
+      ephemeral: true,
+    });
 
     console.log("Nu är vi i interaction grejen");
-    await interaction.reply("Pong!");
   },
 };
