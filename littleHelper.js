d71cd1247b1a4947d9c4ab71aa8de18d78f5ec42
snygghugo <91157834+snygghugo@users.noreply.github.com>
2023-04-27 21:45:49 +0200

    Hehehe

--- littleHelper.js
+++ littleHelper.js
@@ -1,34 +1,25 @@
 const express = require("express");
 
 const app = express();
-const queue = [];
+const queue = ["<@207840759087497217>"];
 
 app.use(express.json());
 
-app.get("/api/developers", (req, res) => {
+app.get("/queue", (req, res) => {
   res.json(db);
 });
 
-app.get("/api/developers/:id", (req, res) => {
-  const paramId = req.params.id;
-  const dev = db.find((e) => e.id == paramId);
-  return dev ? res.json(dev) : res.status(404).end();
-});
-
-app.post("/api/developers/", (req, res) => {
+app.post("/queue", (req, res) => {
   const dev = {
     id: db.length + 1,
     name: req.body.name,
     email: req.body.email,
   };
   db.push(dev);
-  res
-    .status(201)
-    .header("location", `api/developers/${dev.id}`)
-    .json(dev)
-    .end();
+  res.status(201).header("location", `queue`).json(dev).end();
 });
 
-module.exports = {
-  app,
-};
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
