b42363b39660ea56d0435684b511b9389939a38c
snygghugo <91157834+snygghugo@users.noreply.github.com>
2023-04-28 00:36:38 +0200

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -7,31 +7,28 @@ module.exports = {
     .setDescription("Time to queue"),
   async execute(interaction) {
     const queuer = { id: interaction.user.toString() };
-    console.log("Här är queuer");
-    console.log(queuer);
-    const request = {
-      baseURL: "http://localhost:3000/",
-      url: "queue",
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: queuer,
-      responseType: "json",
-    };
-    try {
-      const res = await axios(request);
-      console.log(res);
-      return res;
-    } catch (error) {
-      console.error(error);
-    }
-
-    console.log("Nu är vi i interaction grejen");
-
+    const queue = postQueue(queuer);
     await interaction.reply({
-      content: "The queue looks like this: " + request.data.join("\n"),
+      content: "The queue looks like this: " + queue.data.join("\n"),
       ephemeral: true,
     });
   },
+};
+const postQueue = async (queuer) => {
+  const request = {
+    baseURL: "http://localhost:3000/",
+    url: "queue",
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: queuer,
+    responseType: "json",
+  };
+  try {
+    const res = await axios(request);
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
