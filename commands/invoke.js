ed6aaba0ea0ed2d9777b38818a2702f828043a50
snygghugo <91157834+snygghugo@users.noreply.github.com>
2023-04-28 21:06:50 +0200

    Update invoke.js

--- commands/invoke.js
+++ commands/invoke.js
@@ -8,9 +8,44 @@ module.exports = {
   async execute(interaction) {
     const queuer = { id: interaction.user.toString() };
     const queue = await helpMeLittleHelper(queuer, "get");
-    console.log(queue.data);
-    await interaction.reply({
+    const unreadiedArr = queue.data;
+    const readiedArr = [];
+    console.log(unreadiedArr);
+
+    const buttonRow = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("rdyQueue")
+        .setLabel("✅")
+        .setStyle(ButtonStyle.Success)
+    );
+    const message = await interaction.reply({
       content: `${queue.data.join(", ")} you are being summoned.`,
+      components: buttonRow,
+    });
+
+    const filter = (i) =>
+      i.channel.id === message.channel.id && i.customId === "rdyQueue";
+    unreadiedArr.includes(i.user.id.toString());
+    const collector = message.channel.createMessageComponentCollector({
+      filter,
+      time: pickTime * 1000, //NEED NEW PICK TIME (fem minuter)
+      max: 1,
+    });
+    collector.on("collect", async (i) => {
+      console.log(i);
+      console.log(i.user.username);
+      //The interaction will be "failed" unless we do something with it
+
+      await i.deferReply();
+      await i.deleteReply();
+    });
+
+    collector.on("end", async (collected) => {
+      try {
+      } catch (error) {
+        message.edit("There was an error baby  " + error);
+        console.log(error);
+      }
     });
   },
 };
